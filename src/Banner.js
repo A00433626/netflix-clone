93cbfb5791afefad72afea8f806ccef34886106a
A00433626 <Vidhyadhara.Teja.Katragadda@smu.ca>
2020-08-17 17:30:15 -0300

    add github pages config

--- src/Banner.js
+++ src/Banner.js
@@ -20,12 +20,16 @@ const Banner = () => {
   function truncate(str, n) {
     return str?.length > n ? str.substr(0, n - 1) + "..." : str;
   }
+  console.log(movie.backdrop_path);
+  // const path =  `https:image.tmdb.org/t/p/original${movie?.backdrop_path}`;
+  const path = `https:image.tmdb.org/t/p/original${movie?.backdrop_path}`;
+  console.log(path);
   return (
     <header
       className="banner"
       style={{
         backgroundSize: "cover",
-        backgroundImage: `url("https:image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        backgroundImage: `url("${path}")`,
         backgroundPosition: "center center",
       }}
     >
