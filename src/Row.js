f9587ac7689ed1f6acc2ed4180c81b7a435519cd
A00433626 <Vidhyadhara.Teja.Katragadda@smu.ca>
2020-08-16 21:22:03 -0300

    Added movie-trailer and youtube component

--- src/Row.js
+++ src/Row.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 const base_URL = "https://image.tmdb.org/t/p/original/";
 const Row = (props) => {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   //loads the movies using TMBD API
   // if we use [] then it'll load only once when the page load [moovies] helps to load when ever movies object got updated
@@ -16,7 +19,24 @@ const Row = (props) => {
     fetchMovieData();
   }, [props.fetchUrl]);
   console.log(movies);
-
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+  };
   return (
     <div className="row">
       {/* title*/}
@@ -34,10 +54,12 @@ const Row = (props) => {
                 props.isLargeRow ? movie.poster_path : movie.backdrop_path
               }`}
               alt={movie.name}
+              onClick={() => handleClick(movie)}
             />
           );
         })}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} pts={opts} />}
     </div>
   );
 };
